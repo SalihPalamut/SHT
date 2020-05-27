b27116bb04f5c83f33df3e3e09aaadffb273e826
Salih PALAMUT <palamutsalih@gmail.com>
2020-05-27 10:04:19 +0300

    Copy Paste Hid Class Codes

---
+++ Classes/HID.cs
@@ -0,0 +1,245 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SHT
+{
+    //https://antiduh.com/blog/node/9
+    //https://github.com/mentatpsi/Microchip/tree/master/USB/Device%20-%20HID%20-%20Custom%20Demos/PnP%20Demo%20-%20Windows%20Software/Microsoft%20Visual%20CSharp%202005%20Express
+    class HID
+    {
+        #region "Definations"
+        //Globally Unique Identifier (GUID) for HID class devices.  Windows uses GUIDs to identify things.
+        Guid InterfaceClassGuid = new Guid(0x4d1e55b2, 0xf16f, 0x11cf, 0x88, 0xcb, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30);
+        //Constant definitions from setupapi.h, which we aren't allowed to include directly since this is C#
+        internal const uint DIGCF_PRESENT = 0x02;
+        internal const uint DIGCF_ALLCLASSES = 0x04;
+        internal const uint DIGCF_PROFILE = 0x08;
+        internal const uint DIGCF_DEVICEINTERFACE = 0x10;
+        //Constants for CreateFile() and other file I/O functions
+        internal const short FILE_ATTRIBUTE_NORMAL = 0x80;
+        internal const short INVALID_HANDLE_VALUE = -1;
+        internal const uint GENERIC_READ = 0x80000000;
+        internal const uint GENERIC_WRITE = 0x40000000;
+        internal const uint CREATE_NEW = 1;
+        internal const uint CREATE_ALWAYS = 2;
+        internal const uint OPEN_EXISTING = 3;
+        internal const uint FILE_SHARE_READ = 0x00000001;
+        internal const uint FILE_SHARE_WRITE = 0x00000002;
+
+        //Other constant definitions
+        internal const uint DBT_DEVTYP_DEVICEINTERFACE = 0x05;
+        internal const uint DEVICE_NOTIFY_WINDOW_HANDLE = 0x00;
+        internal const uint ERROR_SUCCESS = 0x00;
+        internal const uint ERROR_NO_MORE_ITEMS = 0x00000103;
+        internal const uint SPDRP_HARDWAREID = 0x00000001;
+
+        //Various structure definitions for structures that this code will be using
+        internal struct SP_DEVICE_INTERFACE_DATA
+        {
+            internal uint cbSize;               //DWORD
+            internal Guid InterfaceClassGuid;   //GUID
+            internal uint Flags;                //DWORD
+            internal uint Reserved;             //ULONG_PTR MSDN says ULONG_PTR is "typedef unsigned __int3264 ULONG_PTR;"  
+        }
+
+        internal struct SP_DEVICE_INTERFACE_DETAIL_DATA
+        {
+            internal uint cbSize;               //DWORD
+            internal char[] DevicePath;         //TCHAR array of any size
+        }
+
+        internal struct SP_DEVINFO_DATA
+        {
+            internal uint cbSize;       //DWORD
+            internal Guid ClassGuid;    //GUID
+            internal uint DevInst;      //DWORD
+            internal uint Reserved;     //ULONG_PTR  MSDN says ULONG_PTR is "typedef unsigned __int3264 ULONG_PTR;"  
+        }
+
+        internal struct DEV_BROADCAST_DEVICEINTERFACE
+        {
+            internal uint dbcc_size;            //DWORD
+            internal uint dbcc_devicetype;      //DWORD
+            internal uint dbcc_reserved;        //DWORD
+            internal Guid dbcc_classguid;       //GUID
+            internal char[] dbcc_name;          //TCHAR array
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HIDP_CAPS
+        {
+            public short Usage;
+            public short UsagePage;
+            public short InputReport;
+            public short OutputReport;
+            public short FeatureReport;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 17)]
+            public short[] Reserved;
+            public short NumberLinkCollectionNodes;
+            public short NumberInputButtonCaps;
+            public short NumberInputValueCaps;
+            public short NumberInputDataIndices;
+            public short NumberOutputButtonCaps;
+            public short NumberOutputValueCaps;
+            public short NumberOutputDataIndices;
+            public short NumberFeatureButtonCaps;
+            public short NumberFeatureValueCaps;
+            public short NumberFeatureDataIndices;
+
+        }
+        public struct HIDD_ATTRIBUTES
+        {
+            public Int32 Size;
+            public Int16 VendorID;
+            public Int16 ProductID;
+            public Int16 VersionNumber;
+
+        }
+        #endregion
+        #region "Dll Imports"
+
+        //DLL Imports.  Need these to access various C style unmanaged functions contained in their respective DLL files.
+        //--------------------------------------------------------------------------------------------------------------
+        // HidD_GetAttributes
+        [DllImport("hid.dll", SetLastError = true)]
+        public static extern Boolean HidD_GetAttributes(
+            SafeFileHandle HidDeviceObject,
+            ref HIDD_ATTRIBUTES Attributes);
+        //HidD_FreePreparsedData
+        [DllImport("hid.dll", SetLastError = true)]
+        public static extern bool HidD_FreePreparsedData(ref IntPtr PreparsedData);
+        // HidD_GetPreparsedData
+        [DllImport("hid.dll", SetLastError = true)]
+        public static extern bool HidD_GetPreparsedData(SafeFileHandle HidDeviceObject, ref IntPtr PreparsedData);
+        //HidP_GetCaps
+        [DllImport("hid.dll", SetLastError = true)]
+        public static extern int HidP_GetCaps(IntPtr preparsedData, ref HIDP_CAPS capabilities);
+        //Returns a HDEVINFO type for a device information set.  We will need the 
+        //HDEVINFO as in input parameter for calling many of the other SetupDixxx() functions.
+        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern IntPtr SetupDiGetClassDevs(
+            ref Guid ClassGuid,     //LPGUID    Input: Need to supply the class GUID. 
+            IntPtr Enumerator,      //PCTSTR    Input: Use NULL here, not important for our purposes
+            IntPtr hwndParent,      //HWND      Input: Use NULL here, not important for our purposes
+            uint Flags);            //DWORD     Input: Flags describing what kind of filtering to use.
+
+        //Gives us "PSP_DEVICE_INTERFACE_DATA" which contains the Interface specific GUID (different
+        //from class GUID).  We need the interface GUID to get the device path.
+        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern bool SetupDiEnumDeviceInterfaces(
+            IntPtr DeviceInfoSet,           //Input: Give it the HDEVINFO we got from SetupDiGetClassDevs()
+            IntPtr DeviceInfoData,          //Input (optional)
+            ref Guid InterfaceClassGuid,    //Input 
+            uint MemberIndex,               //Input: "Index" of the device you are interested in getting the path for.
+            ref SP_DEVICE_INTERFACE_DATA DeviceInterfaceData);    //Output: This function fills in an "SP_DEVICE_INTERFACE_DATA" structure.
+
+        //SetupDiDestroyDeviceInfoList() frees up memory by destroying a DeviceInfoList
+        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern bool SetupDiDestroyDeviceInfoList(
+            IntPtr DeviceInfoSet);          //Input: Give it a handle to a device info list to deallocate from RAM.
+
+        //SetupDiEnumDeviceInfo() fills in an "SP_DEVINFO_DATA" structure, which we need for SetupDiGetDeviceRegistryProperty()
+        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern bool SetupDiEnumDeviceInfo(
+            IntPtr DeviceInfoSet,
+            uint MemberIndex,
+            ref SP_DEVINFO_DATA DeviceInterfaceData);
+
+        //SetupDiGetDeviceRegistryProperty() gives us the hardware ID, which we use to check to see if it has matching VID/PID
+        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern bool SetupDiGetDeviceRegistryProperty(
+            IntPtr DeviceInfoSet,
+            ref SP_DEVINFO_DATA DeviceInfoData,
+            uint Property,
+            ref uint PropertyRegDataType,
+            IntPtr PropertyBuffer,
+            uint PropertyBufferSize,
+            ref uint RequiredSize);
+
+        //SetupDiGetDeviceInterfaceDetail() gives us a device path, which is needed before CreateFile() can be used.
+        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern bool SetupDiGetDeviceInterfaceDetail(
+            IntPtr DeviceInfoSet,                   //Input: Wants HDEVINFO which can be obtained from SetupDiGetClassDevs()
+            ref SP_DEVICE_INTERFACE_DATA DeviceInterfaceData,                    //Input: Pointer to an structure which defines the device interface.  
+            IntPtr DeviceInterfaceDetailData,      //Output: Pointer to a SP_DEVICE_INTERFACE_DETAIL_DATA structure, which will receive the device path.
+            uint DeviceInterfaceDetailDataSize,     //Input: Number of bytes to retrieve.
+            ref uint RequiredSize,                  //Output (optional): The number of bytes needed to hold the entire struct 
+            IntPtr DeviceInfoData);                 //Output (optional): Pointer to a SP_DEVINFO_DATA structure
+
+        //Overload for SetupDiGetDeviceInterfaceDetail().  Need this one since we can't pass NULL pointers directly in C#.
+        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern bool SetupDiGetDeviceInterfaceDetail(
+            IntPtr DeviceInfoSet,                   //Input: Wants HDEVINFO which can be obtained from SetupDiGetClassDevs()
+            ref SP_DEVICE_INTERFACE_DATA DeviceInterfaceData,               //Input: Pointer to an structure which defines the device interface.  
+            IntPtr DeviceInterfaceDetailData,       //Output: Pointer to a SP_DEVICE_INTERFACE_DETAIL_DATA structure, which will contain the device path.
+            uint DeviceInterfaceDetailDataSize,     //Input: Number of bytes to retrieve.
+            IntPtr RequiredSize,                    //Output (optional): Pointer to a DWORD to tell you the number of bytes needed to hold the entire struct 
+            IntPtr DeviceInfoData);                 //Output (optional): Pointer to a SP_DEVINFO_DATA structure
+
+        //Need this function for receiving all of the WM_DEVICECHANGE messages.  See MSDN documentation for
+        //description of what this function does/how to use it. Note: name is remapped "RegisterDeviceNotificationUM" to
+        //avoid possible build error conflicts.
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern IntPtr RegisterDeviceNotification(
+            IntPtr hRecipient,
+            IntPtr NotificationFilter,
+            uint Flags);
+
+        //Takes in a device path and opens a handle to the device.
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern SafeFileHandle CreateFile(
+            string lpFileName,
+            uint dwDesiredAccess,
+            uint dwShareMode,
+            IntPtr lpSecurityAttributes,
+            uint dwCreationDisposition,
+            uint dwFlagsAndAttributes,
+            IntPtr hTemplateFile);
+
+        //Uses a handle (created with CreateFile()), and lets us write USB data to the device.
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern bool WriteFile(
+            SafeFileHandle hFile,
+            byte[] lpBuffer,
+            uint nNumberOfBytesToWrite,
+            ref uint lpNumberOfBytesWritten,
+            IntPtr lpOverlapped);
+
+        //Uses a handle (created with CreateFile()), and lets us read USB data from the device.
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern bool ReadFile(
+            SafeFileHandle hFile,
+            IntPtr lpBuffer,
+            uint nNumberOfBytesToRead,
+            ref uint lpNumberOfBytesRead,
+            IntPtr lpOverlapped);
+        #endregion
+
+        public HID(IntPtr Handle)
+
+        {
+            HidInit(Handle);
+        }
+        private void HidInit(IntPtr Handle)
+        {
+            //Additional constructor code
+
+            //Register for WM_DEVICECHANGE notifications.  This code uses these messages to detect plug and play connection/disconnection events for USB devices
+            DEV_BROADCAST_DEVICEINTERFACE DeviceBroadcastHeader = new DEV_BROADCAST_DEVICEINTERFACE();
+            DeviceBroadcastHeader.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
+            DeviceBroadcastHeader.dbcc_size = (uint)Marshal.SizeOf(DeviceBroadcastHeader);
+            DeviceBroadcastHeader.dbcc_reserved = 0;	//Reserved says not to use...
+            DeviceBroadcastHeader.dbcc_classguid = InterfaceClassGuid;
+
+            //Need to get the address of the DeviceBroadcastHeader to call RegisterDeviceNotification(), but
+            //can't use "&DeviceBroadcastHeader".  Instead, using a roundabout means to get the address by 
+            //making a duplicate copy using Marshal.StructureToPtr().
+            IntPtr pDeviceBroadcastHeader = IntPtr.Zero;  //Make a pointer.
+            pDeviceBroadcastHeader = Marshal.AllocHGlobal(Marshal.SizeOf(DeviceBroadcastHeader)); //allocate memory for a new DEV_BROADCAST_DEVICEINTERFACE structure, and return the address 
+            Marshal.StructureToPtr(DeviceBroadcastHeader, pDeviceBroadcastHeader, false);  //Copies the DeviceBroadcastHeader structure into the memory already allocated at DeviceBroadcastHeaderWithPointer
+            RegisterDeviceNotification(Handle, pDeviceBroadcastHeader, DEVICE_NOTIFY_WINDOW_HANDLE);
+        }
+    }
+}
--- SHT.cs
+++ SHT.cs
@@ -6,22 +6,50 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+using static SHT.HID;
 namespace SHT
 {
     public partial class SHT : Form
     {
+        #region "Initialize"
         public SHT()
         {
             InitializeComponent();
             Icon = Resources.AppIcon;
+            HID UsbHid=new HID(this.Handle);
         }
 
         private void SHT_Load(object sender, EventArgs e)
         {
 
         }
+        #endregion
+        #region "WndProc() "
+        //Constant definitions for certain WM_DEVICECHANGE messages
+        internal const uint WM_DEVICECHANGE = 0x0219;
+        internal const uint DBT_DEVICEARRIVAL = 0x8000;
+        internal const uint DBT_DEVICEREMOVEPENDING = 0x8003;
+        internal const uint DBT_DEVICEREMOVECOMPLETE = 0x8004;
+        internal const uint DBT_CONFIGCHANGED = 0x0018;
+        //This is a callback function that gets called when a Windows message is received by the form.
+        //We will receive various different types of messages, but the ones we really want to use are the WM_DEVICECHANGE messages.
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_DEVICECHANGE)
+            {
+                //Device Add or Remove
+                if (((int)m.WParam == DBT_DEVICEARRIVAL) || ((int)m.WParam == DBT_DEVICEREMOVEPENDING) || ((int)m.WParam == DBT_DEVICEREMOVECOMPLETE) || ((int)m.WParam == DBT_CONFIGCHANGED))
+                {
+                    
+                }
+            } //end of: if(m.Msg == WM_DEVICECHANGE)
 
+            base.WndProc(ref m);
+        } //end of: WndProc() function
+          //-------------------------------------------------------END CUT AND PASTE BLOCK-------------------------------------------------------------------------------------
+          //-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        #endregion
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
             About abt = new About();
